a86b39c1a404375e70425630d933b2d34a7cc06b
Amit Prabhu <amit@Amits-MacBook-Pro.local>
2014-09-30 11:05:56 -0700

    M101P Homework 3.1

---
+++ 3/homework31.py
@@ -0,0 +1,29 @@
+import pymongo
+
+# Create connection to mongodb
+connection = pymongo.Connection("mongodb://localhost",safe=True)
+
+#switch db
+db = connection.school
+
+#get students collection
+students = db.students.find()
+
+#iterate through students
+for item in students :
+    curr_score = -1
+    scores_array = item["scores"]
+    new_scores_array = []
+    for value in scores_array:
+        print "value", value
+        if value["type"] == "homework":
+            if curr_score == -1 :
+                curr_score = value["score"]
+            else:
+                if curr_score < value["score"]:
+                    curr_score = value["score"]
+        else:
+            new_scores_array.append(value)
+
+    new_scores_array.append({"type": "homework","score":curr_score})
+    db.students.update({"_id": item["_id"]},{"$set": {"scores": new_scores_array}})
